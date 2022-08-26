add0638495f37e7901fdea6309a594c09a2e2f81
EmaleeSoto <emaleesoto@gmail.com>
2022-08-26 03:20:19 -0400

    Added gamers controller with index route

---
+++ back-end/controllers/gamerController.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const gamers = express.Router();
+const { getAllGamers } = requrie("../queries/gamers.js");
+
+// INDEX
+gamers.get("/", async (req, res) => {
+  const allGamers = await getAllGamers();
+  if (allGamers[0]) {
+    res.status(200).json({ payload: allGamers });
+  } else {
+    res.status(500).json({ error: "server error" });
+  }
+});
+
+module.exports = gamers;
